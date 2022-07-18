6320054a10bba6c3ac2ca59b5d58cba13ec45a23
MBicalho <77859700+MBicalho@users.noreply.github.com>
2022-07-17 21:32:25 -0300

    Update sql_queries.py
    Adicionado update na coluna level em caso de conflito

--- Projeto1/sql_queries.py
+++ Projeto1/sql_queries.py
@@ -109,7 +109,8 @@ INSERT INTO users
                      level)
 VALUES(%s, %s, %s, %s, %s)
 ON CONFLICT (user_id)
-DO NOTHING
+DO UPDATE 
+SET level = EXCLUDED.level;
 """)
     
 song_table_insert = ("""
