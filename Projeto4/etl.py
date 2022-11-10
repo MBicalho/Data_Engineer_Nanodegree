dcb23e6118d5db15cd81fdb929b4f6a0a66350bc
Matheus Bicalho <77859700+MBicalho@users.noreply.github.com>
2022-11-09 23:18:18 -0300

    Adicionando caminho do s3

--- Projeto4/etl.py
+++ Projeto4/etl.py
@@ -131,7 +131,7 @@ def main():
     
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = "s3://data-lake-matheus/matheus-s3/"
+    output_data = "s3a://udacity-matheus-data-lake/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
