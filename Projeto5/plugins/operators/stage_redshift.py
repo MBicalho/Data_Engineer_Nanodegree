2fcc6167bbfebac802ec98c30a979d8918c79890
Matheus Bicalho <77859700+MBicalho@users.noreply.github.com>
2022-11-13 22:49:14 -0300

    Atualizando aws_credentials

--- Projeto5/plugins/operators/stage_redshift.py
+++ Projeto5/plugins/operators/stage_redshift.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -11,8 +12,7 @@ class StageToRedshiftOperator(BaseOperator):
                  bucket,
                  path,
                  json,
-                 aws_secret,
-                 aws_key,
+                 aws_credentials
                  aws_region,
                  conn_id
                  *args, **kwargs):
@@ -22,8 +22,7 @@ class StageToRedshiftOperator(BaseOperator):
         self.bucket = bucket
         self.path = path
         self.json = json
-        self.aws_secret = aws_secret
-        self.aws_key = aws_key
+        self.aws_credentials = aws_credentials
         self.aws_region = aws_region
         self.conn_id = conn_id
 
@@ -32,6 +31,9 @@ class StageToRedshiftOperator(BaseOperator):
 
         redshift = PostgresHook(postgres_conn_id=self.conn_id)
         
+        hook = AwsHook(self.aws_credentials)
+        credentials = hook.get_credentials()
+        
         #delete data for the table
         
         redshift.run(f"TRUNCATE TABLE {self.myTable}")
@@ -39,8 +41,8 @@ class StageToRedshiftOperator(BaseOperator):
         
         redshift.run(f"COPY {self.myTable}
                         FROM 's3://{self.bucket}/{self.path}'   \
-                        ACCESS_KEY_ID '{self.aws_key}'          \
-                        SECRET_ACCESS_KEY '{self.aws_secret}'   \
+                        ACCESS_KEY_ID '{credentials.access_key}'          \
+                        SECRET_ACCESS_KEY '{credentials.secret_key}'   \
                         REGION '{self.aws_region}'              \
                         JSON '{self.json}'
                      "
