9f11f55a58ca1889d65451b27ffe0a8c7b3f3882
Matheus Bicalho <77859700+MBicalho@users.noreply.github.com>
2022-11-27 18:23:14 -0300

    Update data_quality.py

--- Projeto5/plugins/operators/data_quality.py
+++ Projeto5/plugins/operators/data_quality.py
@@ -10,27 +10,32 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  conn_id,
                  myTable=[],
+                 dq_checks=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
 
         self.conn_id = conn_id
         self.myTable = myTable
+        self.dq_checks = dq_checks
 
     def execute(self, context):
         
         redshift = PostgresHook(postgres_conn_id=self.conn_id)
         
         for myTables in self.myTable:
-            table = redshit.get_records(f"SELECT COUNT(*) FROM {myTables}")
             
-            if (len(table) <= 0 or len(table[0]) <= 0):
-                raise ValueError (f"The table {myTables} returns with none result")
-                
-            
-            num = table[0][0]
-            
-            if (num == 0):
-                raise ValueError (f"Data Quality {myTables} failed!")
-                
-            self.log.info(f"The table data quality passed")
+            for checks in self.dq_checks:
+
+                table = redshit.get_records(f"{checks.get('check_sql')}{myTables}")
+
+                if (len(table) < 1 or len(table[0]) < 1):
+                    raise ValueError (f"The table {myTables} returns with none result")
+
+
+                num = table[0][0]
+
+                if (num == 0):
+                    raise ValueError (f"Data Quality {myTables} failed!")
+
+                self.log.info(f"The table data quality passed")
